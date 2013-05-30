c6963179e2c16ec47251120a7e30eec7e04b8dfe
Paul Sladen <impositor@paul.sladen.org>
2013-05-30 10:49:50 +0100

    fix hex-string bug; hopefully handle zero-append round-tripping

--- parse.py
+++ parse.py
@@ -5,28 +5,44 @@
 import sys
 
 class Token():
-    def __init__(self, start, end, what):
+    def __init__(self, start, end, what, **kwargs):
         self.start = start
         self.end = end
         self.what = what
+        self.__dict__.update(kwargs)
     def __repr__(self):
         return '%s(%d,%d,%s)\n' % (self.__class__.__name__, self.start, self.end, `self.what`)
+    def __str__(self):
+        return self.what
 
 class Whitespace(Token):
+    pass
+class Regular(Token):
     pass
 class Keyword(Token):
     pass
 class String(Token):
+    def __str__(self):
+        return '(' + self.what + ')'
     pass
 class Delimiter(Token):
     pass
 class Name(Token):
-    pass
+    def __str__(self):
+        return '/' + self.what
 class HexString(String):
-    pass
+    def __str__(self):
+        e = self.what.encode('hex')
+        # full-circle auto zero append
+        if self.__dict__.has_key('zero_append') and self.zero_append == True:
+            assert e[-1] == '0'
+            e = e[:-1]
+        return '<' + self.what.encode('hex') + '>'
 class Comment(Token):
     pass
 class Stream(String):
+    def __str__(self):
+        return self.what
     pass
 
 class Tokeniser():
@@ -37,9 +53,9 @@ class Tokeniser():
         i = start
         last = i - 1
         while i < end:
-            print '...', `text[i]`, i
+            #print '...', `text[i]`, i
             assert last < i
-            if len(self.found) > 0: print self.found[-1]
+            #if len(self.found) > 0: print self.found[-1]
             last = i
             if text[i] in self.whitespace:
                 whitespace = ''
@@ -59,7 +75,13 @@ class Tokeniser():
                     nested = 1
                     j = i+1
                     while nested > 0:
-                        j = min(text.index(')', j, end), text.index('(', j, end), text.index('\\', j, end))
+                        #print 'Chewing on', `text[i:i+50]`, `string`, `text[j:j+2]`
+                        minimums = [text.index(')', j, end)]
+                        try: minimums.append(text.index('\\', j, end))
+                        except ValueError: pass
+                        try: minimums.append(text.index('(', j, end))
+                        except ValueError: pass
+                        j = min(minimums)
                         string += text[i+1:j]
                         if text[j] in '()':
                             nested += [1, -1]['()'.index(text[j])]
@@ -67,6 +89,7 @@ class Tokeniser():
                                 string += text[j]
                             j += 1
                         elif text[j] == '\\':
+                            #print 'Working on', `text[i:i+50]`, `string`
                             j += 1
                             if text[j] in 'nrtbf()\\':
                                 # escapes
@@ -100,8 +123,10 @@ class Tokeniser():
                     else:
                         # hexstring
                         j = text.index('>', i, end)
-                        hexstring = (text[i+1:j-1] + '0' * ((j - i) & 1)).decode('hex')
-                        self.found.append(HexString(i, j, hexstring))
+                        print >>sys.stderr, `text[i+1:j]`
+                        zero_append = (j - 1 - i) & 1
+                        hexstring = (text[i+1:j] + '0' * zero_append).decode('hex')
+                        self.found.append(HexString(i, j, hexstring, zero_append=zero_append))
                         i = j + 1
                 elif text[i] is '>':
                     if text[i+1] is '>':
@@ -149,8 +174,10 @@ class Tokeniser():
                     # stream contents, \r follower not allowed
                     # currently this does not strip the newline off the front
                     assert text[i] == '\n' or text[i:i+2] == '\r\n'
-                    j = text.index('\nendstream', i, end)
-                    self.found.append(Stream(i, j, text[i:j]))
+                    extent = 1 + text.index('\n',i,i+2)
+                    self.found.append(Whitespace(i, extent, text[i:extent]))
+                    j = text.index('\nendstream', extent, end)
+                    self.found.append(Stream(extent, j, text[extent:j]))
                     i = j
  
 class impositor:
@@ -162,14 +189,14 @@ class impositor:
         self.pdf_version = float(self.contents[5:8])
         self.pdf_startxref = self.contents.rindex('startxref')
         self.pdf_xref_offset = int(self.contents[self.pdf_startxref + len('startxref'):-len('%%EOF\n')])
-        try:
-            self.t.go(self.contents, self.pdf_xref_offset, self.pdf_startxref)
-        except AssertionError:
-            pass
-        try:
-            self.t.go(self.contents, 2434608-202, len(self.contents))
-        finally:
-            print self.t.found[-10:]
+        #try:
+        #    self.t.go(self.contents, self.pdf_xref_offset, self.pdf_startxref)
+        #except AssertionError:
+        #    pass
+        self.t.go(self.contents, 0, len(self.contents))
+        for f in self.t.found:
+            sys.stdout.write(str(f))
+        pass
     
 def main():
     i = impositor()
